37b650caad163fb7fbd0c3aa35c888deff35207a
nabarun <nabarun@deepstacksoft.com>
2023-05-02 16:45:56 +0530

    Transpile typescript and setup lint

--- packages/gateway-server/src/gateway.ts
+++ packages/gateway-server/src/gateway.ts
@@ -9,9 +9,9 @@ import { buildHTTPExecutor } from '@graphql-tools/executor-http';
 import { stitchSchemas } from '@graphql-tools/stitch';
 import { RenameRootFields, schemaFromExecutor } from '@graphql-tools/wrap';
 
-import watchers from './watchers.json'
+import watchers from './watchers.json';
 
-async function makeGatewaySchema() {
+async function makeGatewaySchema () {
   await isReachable(watchers.map(watcher => watcher.endpoint));
 
   const subSchemaPromises = watchers.map(async watcher => {
@@ -19,7 +19,7 @@ async function makeGatewaySchema() {
     // these are simple functions that query a remote GraphQL API for JSON.
     const remoteExecutor = buildHTTPExecutor({
       endpoint: watcher.endpoint
-    })
+    });
 
     return {
       // Introspect a remote schema. Caveats:
@@ -29,14 +29,14 @@ async function makeGatewaySchema() {
       executor: remoteExecutor,
       transforms: [
         new RenameRootFields(
-          (op, name) => `${watcher.prefix}${name.charAt(0).toUpperCase()}${name.slice(1)}`,
+          (op, name) => `${watcher.prefix}${name.charAt(0).toUpperCase()}${name.slice(1)}`
         )
       ]
-    }
-  })
+    };
+  });
 
   return stitchSchemas({
-    subschemas: await Promise.all(subSchemaPromises),
+    subschemas: await Promise.all(subSchemaPromises)
   });
 }
 
@@ -45,5 +45,5 @@ export const gatewayApp = createYoga({
   maskedErrors: false,
   graphiql: {
     title: 'Azimuth watchers'
-  },
+  }
 });
--- packages/gateway-server/src/index.ts
+++ packages/gateway-server/src/index.ts
@@ -3,8 +3,11 @@
 //
 
 import { createServer } from 'http';
+import debug from 'debug';
 
 import { gatewayApp } from './gateway';
 
+const log = debug('vulcanize:server');
+
 const server = createServer(gatewayApp);
-server.listen(4000, () => console.log('gateway running at http://localhost:4000/graphql'));
+server.listen(4000, () => log('gateway running at http://localhost:4000/graphql'));
