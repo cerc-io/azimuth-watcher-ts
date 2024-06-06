d91216ebf25a089f8a6c13461da7d594f078f4d4
Alessio <alex@playfulpachyderm.com>
2024-06-06 13:42:13 -0700

    Add naive watcher

---
+++ packages/naive-watcher/src/gql/queries/index.ts
@@ -0,0 +1,9 @@
+import fs from 'fs';
+import path from 'path';
+
+export const events = fs.readFileSync(path.join(__dirname, 'events.gql'), 'utf8');
+export const eventsInRange = fs.readFileSync(path.join(__dirname, 'eventsInRange.gql'), 'utf8');
+export const getStateByCID = fs.readFileSync(path.join(__dirname, 'getStateByCID.gql'), 'utf8');
+export const getState = fs.readFileSync(path.join(__dirname, 'getState.gql'), 'utf8');
+export const getSyncStatus = fs.readFileSync(path.join(__dirname, 'getSyncStatus.gql'), 'utf8');
+export const batch = fs.readFileSync(path.join(__dirname, 'batch.gql'), 'utf8');
